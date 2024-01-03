1d0e57ea2e29df9c59172fd67f33349777748cc8
Faizan <faizanansari4862@gmail.com>
2024-01-04 02:15:58 +0530

    host-change

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 // Create a MySQL pool
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || "localhost",
+  host: process.env.MYSQL_HOST || "mysql",
   user: process.env.MYSQL_USER || "admin",
   password: process.env.MYSQL_PASSWORD || "admin",
   database: process.env.MYSQL_DB || "todo_app",
