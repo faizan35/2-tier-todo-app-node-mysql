e284faa09d7a4d4f0797fd01541f29e452e369e8
Faizan <faizanansari4862@gmail.com>
2024-01-04 00:37:16 +0530

    db

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 // Create a MySQL pool
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST || "localhost",
+  user: process.env.MYSQL_USER || "admin",
+  password: process.env.MYSQL_PASSWORD || "admin",
+  database: process.env.MYSQL_DB || "todo_app",
 });
 
 // Export the pool to be used in other modules
