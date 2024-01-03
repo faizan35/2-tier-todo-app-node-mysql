06583d7c29e6239128aac08b450431804767b5d0
Faizan <faizanansari4862@gmail.com>
2024-01-04 01:15:21 +0530

    db

--- database/tasks.sql
+++ database/tasks.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE todo_app;
-USE todo_app;
-
 CREATE TABLE IF NOT EXISTS tasks (
     id INT PRIMARY KEY AUTO_INCREMENT,
     text VARCHAR(255) NOT NULL,
     completed BOOLEAN NOT NULL DEFAULT false
 );
+
+-- CREATE DATABASE todo_app;
+-- USE todo_app;
